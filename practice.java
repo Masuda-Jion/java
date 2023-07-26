dd74b2801745a480c0dc3b19fcc3a5c37cb8ad55
Masuda-Jion <masuda0311jion@gmail.com>
2023-07-26 15:57:49 +0000

    [start] 練習

--- practice.java
+++ practice.java
@@ -1,5 +1,9 @@
+/*pravtice.java
+文字列を表示するプログラム
+2023.7.27*/
+
 class pravtice{
   public static void main(String[] args){
-    System.out.println("Javaを楽しもう！");
+    System.out.println("Javaを楽しもう！"); //画面表示
   }
 }
